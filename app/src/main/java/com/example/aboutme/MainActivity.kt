37287520dc7da247c8eee1e3bbe6303dba628fa8
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-02-03 11:29:39 -0400

    changing bio visibility

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
             edtxtNickname.visibility = View.GONE
             txtGreetings.visibility = View.VISIBLE
             btnDone.visibility = View.GONE
+            txtBio.visibility = View.VISIBLE
         }
     }
 }
