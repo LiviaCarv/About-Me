5421b7e4edede7d6066a99fd69c3f29919913231
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-02-03 08:49:52 -0400

    adding functionality for the done button, displaying a greeting message

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,15 +2,31 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bio: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val doneButton: Button = findViewById(R.id.btn_done)
+        doneButton.setOnClickListener {
+            setNickname(it)
+        }
+
+    }
+
+    private fun setNickname(view: View) {
+        val nicknameTxt: TextView = findViewById(R.id.txt_nickname)
+        val nicknameEditText: EditText = findViewById(R.id.edtxt_nickname)
+        nicknameTxt.text = getString(R.string.greeting_message, nicknameEditText.text)
+        nicknameEditText.visibility = View.GONE
+        nicknameTxt.visibility = View.VISIBLE
+        view.visibility = View.GONE
 
     }
 }
